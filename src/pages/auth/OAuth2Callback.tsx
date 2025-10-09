809bfc21cc74adf38d0c3690d1af602c955a83d9
Trunali-Kansagara <89198770+Trunali-Kansagara@users.noreply.github.com>
2025-10-09 13:29:43 +0530

    merge conflict

---
+++ src/pages/auth/OAuth2Callback.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const OAuth2Callback = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        console.log("OAuth2Callback====",)
+        const params = new URLSearchParams(window.location.search);
+        console.log("params====", params)
+        const accessToken = params.get("accessToken");
+        const refreshToken = params.get("refreshToken");
+        console.log("accessToken---", accessToken, refreshToken)
+        if (accessToken && refreshToken) {
+            localStorage.setItem("accessToken", accessToken);
+            localStorage.setItem("refreshToken", refreshToken);
+
+            // Navigate to dashboard
+            navigate("/dashboard", { replace: true });
+        } else {
+            // No tokens → redirect to login
+            navigate("/", { replace: true });
+        }
+    }, [navigate]);
+
+    return <div>Loading...</div>;
+};
+
+export default OAuth2Callback;
